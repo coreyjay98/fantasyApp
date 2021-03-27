58a2be256f5ba0e6b52ebc0f6e218e30c0a93577
Corey Samuels <coreyjsamuels@gmail.com>
2021-03-27 02:39:05 +0000

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use('/api', require('./server/routes/apiRoutes'));
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 mongoose
